9024e7af41c718060223578cb52e0a69443a844c
admin <tranthidathao234@gmail.com>
2019-10-25 20:04:01 +0700

    projectphp

---
+++ laravel/resources/views/sbadmin2/subviews/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 10/25/2019
+ * Time: 07:47 PM
+ */
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/students', 'Admin\StudentsController@index');
+Route::get('/students/create', 'Admin\StudentsController@create');
+Route::get('/students/{id}/edit', 'Admin\StudentsController@edit');
+Route::get('/students/{id}/delete', 'Admin\StudentsController@delete');
+Route::get('/students', 'Admin\StudentsController@store');
+Route::get('/students/{id}', 'Admin\StudentsController@update');
+Route::get('/students/{id}/delete', 'Admin\StudentsController@destroy');
